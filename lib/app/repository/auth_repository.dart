44ca7803a6ba3047b077e0176c7786b73aedb580
Fernanda <fernanda@inovadora.com.br>
2021-11-08 15:35:37 -0300

    implementado firebase, fazer o ajuste do link no authrepository apos criar bd site firebase.

--- lib/app/repository/auth_repository.dart
+++ lib/app/repository/auth_repository.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:auth/app/auth/model/auth_model.dart';
 import 'package:auth/app/database/connection.dart';
+import 'package:http/http.dart' as http;
 
 class AuthRepository {
   save(AuthModel auth) async {
@@ -21,5 +24,31 @@ class AuthRepository {
         auth.id
       ]);
     }
+    List<Map<String, dynamic>> resultado =
+        await _db.rawQuery('SELECT * FROM auth ORDER BY id DESC LIMIT 1');
+    List<AuthModel> auths = List.generate(resultado.length, (index) {
+      var item = resultado[index];
+      return AuthModel(
+          id: item['id'],
+          name: item['name'],
+          email: item['email'],
+          password: item['password'],
+          confirmPassword: item['confirm_password']);
+    });
+
+    int id = 0;
+    auths.forEach((element) {
+      id = element.id!;
+    });
+
+    final response = await http.post(
+        Uri.parse('https://auth-4f5c3-default-rtdb.firebaseio.com/auth.json'),
+        body: jsonEncode({
+          'id': id,
+          'name': auth.name,
+          'email': auth.email,
+          'password': auth.password,
+          'confirm_password': auth.confirmPassword
+        }));
   }
 }
